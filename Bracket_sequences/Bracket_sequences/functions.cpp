e6d5e25769d38d2341ed1b49a14d4bddfa5ce750
Matvey <matvey.kucherov.2018@mail.ru>
2025-06-23 16:46:01 +0300

    Realization of Decode Function

--- Bracket_sequences/Bracket_sequences/functions.cpp
+++ Bracket_sequences/Bracket_sequences/functions.cpp
@@ -1,6 +1,7 @@
 #include "functions.h"
 #include <set>
 #include <string>
+#include <algorithm> // для reverse
 std::set<int> Generate_level_tree_from_previous(int Number_pairs)
 {
 	std::set<int> d = { 111, 110101, 11001, 1011, 10101 };
@@ -10,9 +11,38 @@ std::set<int> Generate_level_tree_from_previous(int Number_pairs)
 
 std::string Decode(int bit_mask, char open_symbol, char close_symbol) 
 {
-	std::string x = "()";
-	return x;
-}
+    std::string bracket_sequence = "";
+    int balance = 0; // Счётчик открытых скобок
+
+    // Обрабатываем каждую цифру числа (0 или 1)
+    while (bit_mask != 0) {
+        int last_digit = bit_mask % 10; // Получаем последнюю цифру
+        bit_mask /= 10; // Убираем последнюю цифру из числа
+
+        if (last_digit == 1) {
+            bracket_sequence += open_symbol;
+            balance++;
+        }
+        else if (last_digit == 0) {
+            bracket_sequence += close_symbol;
+            balance--;
+        }
+    }
+
+    // Разворачиваем, так как цифры обрабатывались справа налево
+    std::reverse(bracket_sequence.begin(), bracket_sequence.end());
+
+    // Добавляем недостающие закрывающие скобки
+    while (balance > 0) {
+        bracket_sequence += close_symbol;
+        balance--;
+    }
+
+    
+
+    return bracket_sequence;
+    }
+
 
 
 int Add_zeros_and_unit(int bit_mask, int N) 
